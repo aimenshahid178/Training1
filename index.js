d4c41da79fd2e98be31e527d9d5f40fcef23dc18
Aimen Shahid <ashahid@pk.folio3.com>
2020-02-19 12:31:19 +0500

    Drag To End

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let todo_list = [1,2,3,4];
 let id;
-let container = document.querySelector('section');
+//var container = document.querySelector("section");
+var dropTarget = document.querySelector(".box");
+var draggables = document.querySelectorAll(".task");
+
 
 function add_item(item){
     todo_list.push(item);
@@ -14,10 +17,10 @@ function edit_item(id){
     if(change != null){
         document.getElementById(id).innerHTML = change;
     }
-};
+}; /*
 
 function allowDrop(ev){
-    ev.preventDefault();
+   ev.preventDefault();
 };
 
 function dragStart(ev){
@@ -26,7 +29,7 @@ function dragStart(ev){
 
 function drop(ev){
     ev.target.append(document.getElementById(id));
-};
+}; */
 
 function display_items(){
     
@@ -34,14 +37,31 @@ function display_items(){
         let newDiv = document.createElement("div");
         let newContent = document.createTextNode(todo_list[i]);
         newDiv.setAttribute("id", todo_list[i]);
-        newDiv.setAttribute("ondragstart", dragStart(event));
+        //newDiv.setAttribute("ondragstart", dragStart(event));
+        newDiv.setAttribute("class", "task");
         newDiv.setAttribute("draggable",true);
         newDiv.onclick = function(){ newDiv.remove(); };
         newDiv.appendChild(newContent);
-        container.appendChild(newDiv);
+        dropTarget.appendChild(newDiv);
         //var currentDiv = document.getElementById("demo"); 
         //document.body.insertBefore(newDiv, currentDiv);
         document.getElementById(todo_list[i]).addEventListener("contextmenu",function(){edit_item(todo_list[i])});
+        dropTarget.addEventListener("dragstart", function(ev){
+            ev.dataTransfer.setData("srcId", ev.target.id);
+        });
+        dropTarget.addEventListener('dragover', function(ev) {
+            ev.preventDefault();
+          });
+          dropTarget.addEventListener('drop', function(ev) {
+            ev.preventDefault();
+            let target = ev.target;
+            let droppable  = target.classList.contains('box');
+            let srcId = ev.dataTransfer.getData("srcId");
+            
+            if (droppable) {
+              ev.target.appendChild(document.getElementById(srcId));
+            }
+          });
         
     }
 };
@@ -51,11 +71,28 @@ function display_new_item(item){
     let newContent = document.createTextNode(item);
     newDiv.setAttribute("id", todo_list.indexOf(item));
     newDiv.setAttribute("draggable",true);
+    newDiv.setAttribute("class", "task");
     newDiv.onclick = function(){ newDiv.remove(); };
     newDiv.appendChild(newContent);
-    container.appendChild(newDiv);
-    //var currentDiv = document.getElementById("demo"); 
-    //document.body.insertBefore(newDiv, currentDiv);
+    dropTarget.appendChild(newDiv);
     document.getElementById(todo_list.indexOf(item)).addEventListener("contextmenu",function(){edit_item(todo_list.indexOf(item))});
-
+    dropTarget.addEventListener("dragstart", function(ev){
+        ev.dataTransfer.setData("srcId", ev.target.id);
+    });
+    dropTarget.addEventListener('dragover', function(ev) {
+        ev.preventDefault();
+      });
+      dropTarget.addEventListener('drop', function(ev) {
+        ev.preventDefault();
+        let target = ev.target;
+        let droppable  = target.classList.contains('box');
+        let srcId = ev.dataTransfer.getData("srcId");
+        
+        if (droppable) {
+          ev.target.appendChild(document.getElementById(srcId));
+        }
+      });
+    
 }
+
+
